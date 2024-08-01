75d37aebbca24961fc88b41529f40a3f6563a7ce
utatsineza <h.utatsinez@alustudent.com>
2024-08-01 07:54:19 -0700

    python-network_1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+"""Fetches url
+using the request moduel
 """
-Module 4-hbtn_status.py
-"""
-
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
