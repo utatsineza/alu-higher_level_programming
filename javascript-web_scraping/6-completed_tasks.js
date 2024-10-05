9279368cd5fb13da7565f7cba559cc361fc4612f
utatsineza <h.utatsinez@alustudent.com>
2024-10-05 13:01:51 -0700

    changes

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,29 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    const completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+
+function getTasks (data, userId) {
+  let count = 0;
+  data.filter((element) => element.userId === userId)
+    .forEach((getTasks) => {
+      if (getTasks.completed === true) {
+        count++;
       }
     });
-    console.log(completed);
-  }
+  return count;
+}
+
+const url = process.argv[2];
+request.get(url, (error, response) => {
+  if (error) { console.log(error); }
+
+  const data = JSON.parse(response.body);
+  const users = {};
+  data.forEach((element) => {
+    if (users[element.userId] === undefined) {
+      if (getTasks(data, element.userId) > 0) {
+        users[element.userId] = getTasks(data, element.userId);
+      }
+    }
+  });
+  console.log(users);
 });
